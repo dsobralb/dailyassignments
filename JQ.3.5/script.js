93cf60f5ee13b3ff9dbc74c9c97d5fe7ff4df8cb
dsobralb <dsobralb@gmail.com>
2024-10-26 20:21:18 -0600

    completed JQ day 3, part 5

--- JQ.3.5/script.js
+++ JQ.3.5/script.js
@@ -8,3 +8,15 @@ $('#createButton').on('click', function() {
 });
 
 // Write your code here
+$("#createButton").on("click", function() {
+    $("#container").append(`
+        <div class="textContainer">
+            <div class="toggleText">Toggle Text</div>
+            <p class="noDisplay">Surprise! Here is some text.</p>
+        </div>
+    `);
+});
+
+$("#container").on("click", ".toggleText", function() {
+    $(this).next("p").toggleClass("noDisplay");
+});
