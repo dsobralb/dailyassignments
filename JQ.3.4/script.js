4e96d0a5c8ce5fc6c116e3d9255c6ee7ed90f998
dsobralb <dsobralb@gmail.com>
2024-10-26 20:18:50 -0600

    completed JQ day 3, part 4

--- JQ.3.4/script.js
+++ JQ.3.4/script.js
@@ -1,35 +1,17 @@
-//-------------------------------------------------
-// The usual way to use the on click method
-//-------------------------------------------------
-
-$('#newButton').on('click', function() {
-  
-  // When the button is clicked add a whole new counter
-  // to the DOM
-  $('#container').append(`
-    <div class="counterContainer">
-      <div class="counterButton">Click me</div>
-      <div>Counter: <span class="counterText">0</span></div>
-    </div>
-  `);
-  
+// Step 1: Add a new counter container when #newButton is clicked
+$("#newButton").on("click", function() {
+    $("#container").append(`
+        <div class="counterContainer">
+            <div class="counterButton">Click me</div>
+            <div>Counter: <span class="counterText">0</span></div>
+        </div>
+    `);
 });
 
-//-------------------------------------------------
-// Using delegation when items are added to the DOM 
-//-------------------------------------------------
-
-$('#container').on('click', '.counterButton', function() {
-  
-  // This line grabs the sibling that is next to it. So in this
-  // case it grabs the div with the counter
-  let counter = $(this).next();
-  
-  // This line grabs the span that has the actual number part
-  // of the text
-  let counterText = $(counter).children()[0];
-  
-  // Add one to the counter
-  $(counterText).text(Number($(counterText).text()) + 1);
-  
+// Step 2: Use event delegation to handle clicks on dynamically added .counterButton
+$("#container").on("click", ".counterButton", function() {
+    // Get the sibling counter text span and increase its value by 1
+    let counterText = $(this).next().find(".counterText");
+    let currentCount = parseInt(counterText.text());
+    counterText.text(currentCount + 1);
 });
