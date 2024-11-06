171cf8876facc1ded6f978c5a65162b78afe3756
dsobralb <dsobralb@gmail.com>
2024-11-05 21:59:12 -0700

    Completed JS. day 3, part 4

--- JS.3.4/index.js
+++ JS.3.4/index.js
@@ -1,61 +1,68 @@
 // Problem 1
-// Create a function called circleArea.
-// This function will take one parameter which will be a radius.
-// Return the area of the circle based on the radius.
-// area is 3.14 * r^2
+// Function to calculate the area of a circle based on radius.
 
+function circleArea(radius) {
+  return 3.14 * radius ** 2;  // Area formula: π * r^2
+}
 
-
+console.log(circleArea(5));  // Example usage
 
 
 // Problem 2
-// Create a function called circleCircumference.
-// This function will take one parameter which will be a radius.
-// Return the circumference of the circle based on the radius.
-// circumference is 2 * 3.14 * radius
+// Function to calculate the circumference of a circle based on radius.
 
+function circleCircumference(radius) {
+  return 2 * 3.14 * radius;  // Circumference formula: 2 * π * r
+}
 
-
-
+console.log(circleCircumference(5));  // Example usage
 
 
 // Problem 3
-// Create a function called average.
-// This function will take three parameters which will be numbers.
-// Find the average of these numbers and return the value.
+// Function to calculate the average of three numbers.
 
+function average(num1, num2, num3) {
+  return (num1 + num2 + num3) / 3;  // Sum all numbers and divide by 3
+}
 
-
+console.log(average(5, 10, 15));  // Example usage
 
 
 // Problem 4
-// Create a function called combineTwoStrings.
-// This function will take two parameters.
-// return both of the strings concatenated(combined)
+// Function to concatenate two strings.
 
+function combineTwoStrings(str1, str2) {
+  return str1 + str2;  // Concatenate str1 and str2
+}
 
+console.log(combineTwoStrings("Hello, ", "world!"));  // Example usage
 
 
 // Problem 5
-// Create a function called cubed.
-// This function will take one parameter.
-// Return the cubed number that is given from the parameter.
+// Function to cube a given number.
 
+function cubed(num) {
+  return num ** 3;  // Cubes the given number
+}
 
+console.log(cubed(3));  // Example usage
 
 
 // Problem 6
-// Create a function called squared.
-// This function will take one parameter.
-// Return the squared number that is given from the parameter.
+// Function to square a given number.
 
+function squared(num) {
+  return num ** 2;  // Squares the given number
+}
 
+console.log(squared(4));  // Example usage
 
 
 // Problem 7
-// Create a function called sphereArea.
-// This function will take one parameter which will be the radius.
-// Find the surface area given the radius 4 * 3.14 * r^2
-// Use your squared function to find the squared radius.
+// Function to calculate the surface area of a sphere based on radius.
 
+function sphereArea(radius) {
+  return 4 * 3.14 * squared(radius);  // Surface area formula: 4 * π * r^2
+}
 
+console.log(sphereArea(5));  // Example usage
