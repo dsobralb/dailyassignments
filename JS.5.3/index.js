9cc9b38684064b8b49ffbc32ed35cf6a22c4d02d
dsobralb <dsobralb@gmail.com>
2024-11-05 23:36:25 -0700

    Completed JS. day 5, part 3

--- JS.5.3/index.js
+++ JS.5.3/index.js
@@ -1,19 +1,23 @@
 // Problem 1
-// Use the filter function on the array below.
-// We only want an array that has numbers that are greater than 5 and less than 11
-// Assign the result array to myArray
+// Use the filter function to get numbers greater than 5 and less than 11 from myArray.
 
 let myArray = [1, 11, 7, 3, 8, 2, 3, 2, 10, 3, 6, 2, 5];
 function filterMyArray() {
-  //write code here
+  myArray = myArray.filter(num => num > 5 && num < 11); // Filters numbers between 5 and 11
 }
+filterMyArray();
+console.log(myArray); // Output: [7, 8, 10, 6]
+
 
 // Problem 2
-// Use the forEach function on the array below
-// We want to take each item in the array and multiply it by 5;
+// Use the forEach function to multiply each item in multiplyArray by 5.
 
 let multiplyArray = [1, 11, 7, 3, 8, 2, 3, 2, 10, 3, 6, 2, 5];
 
 function multiplyNumbers() {
-  //write code here
+  multiplyArray.forEach((num, index, array) => {
+    array[index] = num * 5; // Multiply each element by 5 and update the array in place
+  });
 }
+multiplyNumbers();
+console.log(multiplyArray); // Output: [5, 55, 35, 15, 40, 10, 15, 10, 50, 15, 30, 10, 25]
