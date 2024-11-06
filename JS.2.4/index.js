97a58e4a6f62231dc1adc1abe202910284b91726
dsobralb <dsobralb@gmail.com>
2024-11-05 21:42:13 -0700

    Completed JS. day 2, pat 4

--- JS.2.4/index.js
+++ JS.2.4/index.js
@@ -1,20 +1,14 @@
-// Open up the instructions first thing!
-
-// Demonstrate a for loop
-// We have a variable addTo. The user will be able to enter any Number
-// to that variable. Create a for loop that loops 
-// 10 times(0-9). Every time the loop runs add the iterator 
-// to the addTo variable and assign the value to the addTo variable
-
-// Note: The iterator is the variable you declare in parenthesis of
-// the for loop. This variable is usually called 'i'.
-
-
 let addTo;
 function adding(myNum) {
   addTo = myNum;
-  
-  //write for loop here
-  
-  
+
+  // Write the for loop here
+  for (let i = 0; i < 10; i++) {
+    addTo += i;  // Adds the current value of i to addTo on each loop
+  }
+
+  console.log(`Final value of addTo: ${addTo}`);
 }
+
+// Example usage
+adding(5);  // You can pass any number to test the function, e.g., 5
