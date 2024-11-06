45d0d04c5a36522d4fecede4f9d37b4b0e1e168b
dsobralb <dsobralb@gmail.com>
2024-11-05 22:08:30 -0700

    Completed JS. day 4, part 1

--- JS.4.1/index.js
+++ JS.4.1/index.js
@@ -1,47 +1,59 @@
 // Problem 1
-// We want to create an object about a person.
-// Give this person a name of 'John'
-// Give this person an age of 23
-// Give this person a height of '5 feet 11 inches'
+// Create an object representing a person with given properties.
 
-let person = {};
+let person = {
+  name: "John",
+  age: 23,
+  height: "5 feet 11 inches"
+};
 
+console.log(person);
 
 
 // Problem 2
-// Lets create a themePark object
-// Our theme park has 8 rollerCoasters
-// 11 kidFriendlyRides
-// 3 shows
-// 40 churroStands
+// Create an object representing a theme park with given properties.
 
-let themePark = {};
+let themePark = {
+  rollerCoasters: 8,
+  kidFriendlyRides: 11,
+  shows: 3,
+  churroStands: 40
+};
+
+console.log(themePark);
 
 
 // Problem 3
-// Lets create a fastFood object
-// This object will tell us what foods are in stock
-// burgers is true
-// fries is true
-// iceCreamMachineBroken is true
-// We just ran out of orangeSoda so make it false.
+// Create an object representing a fast food restaurant's stock status.
 
-let fastFood = {};
+let fastFood = {
+  burgers: true,
+  fries: true,
+  iceCreamMachineBroken: true,
+  orangeSoda: false
+};
 
+console.log(fastFood);
 
 
 // Problem 4
-// Lets create a mathHelp object
-// Give it a function called squared that will take one parameter
-// and have it return the squared number from the parameter.
-// Give it a function called toCelsius.
-// This function takes one parameter which will be a number in fahrenheit
-// to convert to celsius. Return this number.
-// Give it a function called distanceBetweenTwoPoints
-// This function will take 4 parameters in this order:
-// x1,y1,x2,y2
-// Find the distance between the two points given and return the value.
-// Don't know the equation to find the distance? Go to the link
-// under problem 4 help.
+// Create an object with math helper functions.
 
-let mathHelp = {};
+let mathHelp = {
+  squared: function(num) {
+    return num ** 2;  // Returns the square of the parameter
+  },
+  
+  toCelsius: function(fahrenheit) {
+    return (fahrenheit - 32) * (5 / 9);  // Converts Fahrenheit to Celsius
+  },
+  
+  distanceBetweenTwoPoints: function(x1, y1, x2, y2) {
+    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);  // Distance formula
+  }
+};
+
+// Example usage
+console.log(mathHelp.squared(5));  // Example: 25
+console.log(mathHelp.toCelsius(68));  // Example: 20
+console.log(mathHelp.distanceBetweenTwoPoints(0, 0, 3, 4));  // Example: 5
