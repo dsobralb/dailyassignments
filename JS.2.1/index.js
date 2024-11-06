72248c7b2ebab96744f82cca5f1d1b5e3bbc0aca
dsobralb <dsobralb@gmail.com>
2024-11-05 21:29:56 -0700

    Completed JS. day2, pat 1

--- JS.2.1/index.js
+++ JS.2.1/index.js
@@ -6,7 +6,7 @@ let x = 30;
 let y = 20;
 let z = true;
 
-if(x < y) {
+if (x > y) {  // Changed < to >
   z = true;
 } else {
   z = false;
@@ -20,7 +20,7 @@ let a = "10% off!";
 let b = "Love it!";
 let c = true;
 
-if(a === b) {
+if (a !== b) {  // Changed === to !==
   c = true;
 } else {
   c = false;
@@ -34,10 +34,10 @@ let d = 5;
 let e = 5;
 let f = true;
 
-if(d === e) {
-  f = false;
-} else {
+if (d !== e) {  // Changed === to !==
   f = true;
+} else {
+  f = false;
 }
 
 //------------------------
@@ -49,8 +49,8 @@ let h = 50;
 let i = 20;
 let j = true;
 
-if(g >= i) {
-  if(i >= h) {
+if (g <= i) {  // Changed >= to <=
+  if (i <= h) {  // Changed >= to <=
     j = true;
   } else {
     j = false;
@@ -68,16 +68,16 @@ let l = -12;
 let m = "-12";
 let n = true;
 
-if(k > l) {
-  n = false;
-}
-else if(l < k) {
-  n = false;
-}
-else if(l == m) {
-  n = false;
-} else {
+if (k < l) {  // Changed > to <
   n = true;
+}
+else if (l > k) {  // Changed < to >
+  n = true;
+}
+else if (l !== m) {  // Changed == to !==
+  n = true;
+} else {
+  n = false;
 }
 
 //------------------------
@@ -88,7 +88,7 @@ let o = 20;
 let p = 20;
 let q = true;
 
-if(o !== p) {
+if (o === p) {  // Changed !== to ===
   q = true;
 } else {
   q = false;
@@ -98,22 +98,22 @@ if(o !== p) {
 // Problem 7
 //------------------------
 
-let r = 12 < 2 ? true : false;
+let r = 12 > 2 ? true : false;  // Changed < to >
 
 //------------------------
 // Problem 8
 //------------------------
 
-let s = "Hello!" === "Hello!" ? false : true;
+let s = "Hello!" !== "Hello!" ? true : false;  // Changed === to !==
 
 //------------------------
 // Problem 9
 //------------------------
 
-let t = 50 == "50" ? false : true;
+let t = 50 !== "50" ? true : false;  // Changed == to !==
 
 //------------------------
 // Problem 10
 //------------------------
 
-let u = 5 !== 5 ? true : false;
+let u = 5 === 5 ? true : false;  // Changed !== to ===
